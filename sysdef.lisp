55878585db9d163088c453057d02a50ae8a67c48
sross <sross@mudballs.com>
2008-11-19 15:41:37 +0100

    remove sysdef.lisp file
    darcs-hash:20081119144137-8d672-30422f6fbf2814b0bbb0abfd5771a6e2c24ac5f7.gz

--- sysdef.lisp
+++
@@ -1,4 +0,0 @@
-(define-system :gzip-stream (serial-system)
-  (:version 0 2 1)
-  (:components "package" "ifstar" "inflate" "gzip-stream")
-  (:requires :salza2 :flexi-streams :trivial-gray-streams))
